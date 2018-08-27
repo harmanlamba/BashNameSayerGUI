6b6e7387ca708f5ad513dd557af95e83dc1fe8a1
Harman Lamba <harrygth15478@gmail.com>
2018-08-27 17:46:45 +1200

    Fixed the removing creations bug V2.0

--- src/NameSayer/Creations.java
+++ src/NameSayer/Creations.java
@@ -6,7 +6,6 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
 import javafx.scene.control.ListView;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
